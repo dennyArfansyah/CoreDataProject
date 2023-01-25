870e997a13e468e37ec1d2fc8db1677652dd711e
dennyArfansyah <dennyarfansyah@gmail.com>
2023-01-25 23:10:43 +0700

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Denny Arfansyah on 25/01/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
